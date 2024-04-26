e87afb1f6ab30e448f840393dcd8046441b0ac12
SterrenHeer <sterrenheer@gmail.com>
2024-04-26 13:45:10 +0300

    add test props for cost item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import CostItem from "./components/CostItem";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2024, 1, 11),
+      description: 'Холодильник',
+      amount: 999.99
+    },
+    {
+      date: new Date(2024, 11, 25),
+      description: 'Ноутбук',
+      amount: 1254.72
+    },
+    {
+      date: new Date(2024, 4, 1),
+      description: 'Тачка садовая',
+      amount: 40.13
+    }
+  ]
   return (
     <div>
       <h1>Hello World!</h1>
-      <CostItem></CostItem>
+      <CostItem 
+        date={costs[0].date} 
+        description={costs[0].description} 
+        amount={costs[0].amount}></CostItem>
+      <CostItem 
+        date={costs[1].date} 
+        description={costs[1].description} 
+        amount={costs[1].amount}></CostItem>
+      <CostItem 
+        date={costs[2].date} 
+        description={costs[2].description} 
+        amount={costs[2].amount}></CostItem>
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,15 +1,12 @@
 import './CostItem.scss'
 
-function CostItem() {
-  const costDate = new Date(2024, 1, 11);
-  const costDescription = 'Холодильник';
-  const costAmount = 999.99;
+function CostItem(props) {
   return (
     <div className='cost-item'>
-      <div>{costDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className='cost-item_description'>
-        <h2>{costDescription}</h2>
-        <div className='cost-item_price'>${costAmount}</div>
+        <h2>{props.description}</h2>
+        <div className='cost-item_price'>${props.amount}</div>
       </div>
     </div>
   );
