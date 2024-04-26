376d31d9d5a9badb45049d0264e9552d81cb291e
SterrenHeer <sterrenheer@gmail.com>
2024-04-26 13:33:49 +0300

    add static placeholders to cost item

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,12 +1,15 @@
 import './CostItem.scss'
 
 function CostItem() {
+  const costDate = new Date(2024, 1, 11);
+  const costDescription = 'Холодильник';
+  const costAmount = 999.99;
   return (
     <div className='cost-item'>
-      <div>февраль 2024 11</div>
+      <div>{costDate.toDateString()}</div>
       <div className='cost-item_description'>
-        <h2>Холодильник</h2>
-        <div className='cost-item_price'>$999.99</div>
+        <h2>{costDescription}</h2>
+        <div className='cost-item_price'>${costAmount}</div>
       </div>
     </div>
   );
