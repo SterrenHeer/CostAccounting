51fc79fea1bcf18a2d5d88cd8468bc476c4441a9
SterrenHeer <sterrenheer@gmail.com>
2024-05-07 12:59:11 +0300

    change static cost list to dynamic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import React, { useState } from "react";
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
+const INITIAL_COSTS = [
+  {
+    id: "c1",
+    date: new Date(2024, 1, 11),
+    description: 'Холодильник',
+    amount: 999.99
+  },
+  {
+    id: "c2",
+    date: new Date(2024, 11, 25),
+    description: 'Ноутбук',
+    amount: 1254.72
+  },
+  {
+    id: "c3",
+    date: new Date(2024, 4, 1),
+    description: 'Тачка садовая',
+    amount: 40.13
+  }
+]
+
 const App = () => {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2024, 1, 11),
-      description: 'Холодильник',
-      amount: 999.99
-    },
-    {
-      id: "c2",
-      date: new Date(2024, 11, 25),
-      description: 'Ноутбук',
-      amount: 1254.72
-    },
-    {
-      id: "c3",
-      date: new Date(2024, 4, 1),
-      description: 'Тачка садовая',
-      amount: 40.13
-    }
-  ]
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
   const addCostHandler = (cost) => {
-    console.log(cost)
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
   };
 
   return (
